45a3c4c06e25a196e7af8a9cc7df24506d7840d1
Agustin Escalante <agustinescalante27@gmail.com>
2020-05-09 03:38:10 -0300

    Random

---
+++ js/index.js
@@ -0,0 +1,32 @@
+"use strict";
+let resultados_r=["5-0","3-1","1-0","2-1"];
+let resultados_e=["0-0","2-2","1-1","5-5"];
+
+let btn = document.getElementById("boton");
+btn.addEventListener("click", cambiar);
+
+function ran(){
+  let num = Math.floor(Math.random() * resultados_r.length);
+return num;
+  }
+
+function cambiar(){
+      let btn_e = document.getElementById("emp").checked;
+
+      let btn_g = document.getElementById('gan').checked;
+
+      let btn_p = document.getElementById('per').checked;
+
+    if(btn_p){
+        let var_=document.getElementById("textNombre");
+        var_.innerHTML= "Perdemos " + resultados_r[ran()];
+
+  } else if(btn_e){
+        let var_=document.getElementById("textNombre");
+        var_.innerHTML= "Empatamos " + resultados_e[ran()];
+
+    }else if(btn_g){
+        let var_=document.getElementById("textNombre");
+        var_.innerHTML= "Ganamos " + resultados_r[ran()];
+    }
+    }
